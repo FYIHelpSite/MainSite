255d200a35f68169c15afc6bc7e1f8c5dba338db
FYIHelpSite <127453130+FYIHelpSite@users.noreply.github.com>
2023-04-24 10:17:06 -0700

    update

--- cookies.js
+++ cookies.js
@@ -1,14 +1,20 @@
 /* 
 Once someone is on the site this js code will launch the button asking for cookie permission 
 If the cookie already exists on the system then the launcher will not show up as the cookie has already been accepted/denied
-If it is the users first time or the cookie has expired then it will ask the user again if they would like to accept/deny
-The cookies expire at the end of a users session 
+If it is the users first time or the cookie has expired then it will ask the user again if they would like to accept/deny 
+Some cookies expire after the user leaves the site so they onlyn last for the session and the user will be prompted again 
+
 The cookie itself can ask for a user prompt such as a name and this data can be stored in the cookie to be remembered for later
-these are session cookies because they only last during the session 
 There are some platforms like Monsito and One trust that do the cookie part for you and ensure it is compliant, though its not free 
 So far the cookies we create separately are deleted but the google analytics cookies remain and they are not essential 
 GA cookies remain so these analytics appear to stay on the site 
 Now the banner will not appear if the saved cookie exists in the browser 
+
+so the website cookies should be working because as I have checked for the other sites 
+they also keep the _ga, _gat, etc cookies which are all google analytic cookies 
+companies like dell, razer, discord, etc keep these cookies 
+after they delete the non essential cookies such as marketing or ads, but the current site as it is now
+has none of those types of cookies 
 
 github git ignore remove
 
@@ -34,11 +40,22 @@ const buttons = document.querySelectorAll(".button-cookie");
 
 
 const executeCodes = () => {
-// Check if cookie contains coding lab cookie once the website is active
+// Check if cookie contains FYI name check cookie once the website is active
 if (document.cookie.includes("FYI_Name_Check")) {
 cookieBox.classList.remove("show");
-} else {
+} 
+else 
+{
 cookieBox.classList.add("show");
+}
+// checks if fyi example cookie exists 
+if (document.cookie.includes("FYI_Example_Cookie"))
+{
+  cookieBox.classList.remove("show");
+}
+else
+{
+  cookieBox.classList.add("show");
 }
 
 
@@ -55,41 +72,12 @@ if (button.id == "acceptBtn-cookie") {
   // Get the user's name and store it in a cookie
   const name = prompt("Please enter your name:");
   document.cookie = "userName=${name}; expires=0";
-
 } 
-
-else if (button.id == "acceptBtn-cookie")
-{
-    // Set the cookie to expire at the end of the session
-    document.cookie = "cookieBy=; expires=Thu, 01 Jan 1970 00:00:00 PST;";
-    document.cookie = "userName=; expires=Thu, 01 Jan 1970 00:00:00 PST;";
-    document.cookie = "cookieBy=; expires=Thu, 1 Jan 2000 00:00:00 PST;";
-    document.cookie = "cookieBy=; expires=Fri, 01 Jan 2000 00:00:00:00 PST;";
-  
-  /* these cookies are google analytic cookies and they are set to false/null to not track this data */
-  ga('set', 'anonymizeIp', true);
-  ga('set', 'displayFeaturesTask', null);
-  ga('set', 'useAmpClientId', null);
-  ga('set', 'allowAdFeatures', false);
-  ga('set', 'allowLinker', false);
-  gid('set', 'anonymizeIp', true);
-  gid('set', 'displayFeaturesTask', null);
-  gid('set', 'useAmpClientId', null);
-  gid('set', 'allowAdFeatures', false);
-  gid('set', 'allowLinker', false);
-  
-  document.cookie = "_ga=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-  document.cookie = "_gid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-  /* should delete the cookies from the browser */
-  
-}
 
 else {
   // Delete the cookies because this date has already passed 
-  document.cookie = "cookieBy=; expires=Thu, 01 Jan 1970 00:00:00 PST;";
-  document.cookie = "userName=; expires=Thu, 01 Jan 1970 00:00:00 PST;";
-  document.cookie = "cookieBy=; expires=Thu, 1 Jan 2000 00:00:00 PST;";
-  document.cookie = "cookieBy=; expires=Fri, 01 Jan 2000 00:00:00:00 PST;";
+  document.cookie = "cookieBy=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure;";
+  document.cookie = "userName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure;";
 
 /* these cookies are google analytic cookies and they are set to false/null to not track this data */
 ga('set', 'anonymizeIp', true);
@@ -97,15 +85,45 @@ ga('set', 'displayFeaturesTask', null);
 ga('set', 'useAmpClientId', null);
 ga('set', 'allowAdFeatures', false);
 ga('set', 'allowLinker', false);
+
 gid('set', 'anonymizeIp', true);
 gid('set', 'displayFeaturesTask', null);
 gid('set', 'useAmpClientId', null);
 gid('set', 'allowAdFeatures', false);
 gid('set', 'allowLinker', false);
 
-document.cookie = "_ga=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-document.cookie = "_gid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-/* can probably make this into a function */
+gat('set', 'anonymizeIp', true);
+gat('set', 'displayFeaturesTask', null);
+gat('set', 'useAmpClientId', null);
+gat('set', 'allowAdFeatures', false);
+gat('set', 'allowLinker', false);
+
+gtag('set', 'anonymizeIp', true);
+gtag('set', 'displayFeaturesTask', null);
+gtag('set', 'useAmpClientId', null);
+gtag('set', 'allowAdFeatures', false);
+gtag('set', 'allowLinker', false);
+
+JAR('set', 'anonymizeIp', true);
+JAR('set', 'displayFeaturesTask', null);
+JAR('set', 'useAmpClientId', null);
+JAR('set', 'allowAdFeatures', false);
+JAR('set', 'allowLinker', false);
+
+cf('set', 'anonymizeIp', true);
+cf('set', 'displayFeaturesTask', null);
+cf('set', 'useAmpClientId', null);
+cf('set', 'allowAdFeatures', false);
+cf('set', 'allowLinker', false);
+/* _ga, _gid, _gat, _gat_gtag, 1P_JAR __cf_bm */
+
+document.cookie = "_ga=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure;";
+document.cookie = "_gid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure;";
+document.cookie = "_gtag=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure;";
+document.cookie = "_gat; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure;";
+document.cookie = "__cf_bm; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure;";
+document.cookie = "JAR; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure";
+/* google fonts, google analytics, gtag manager, google ads */
 
 }
 });
@@ -116,13 +134,22 @@ document.cookie = "_gid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 function deleteCookies() {
     var theCookies = document.cookie.split(';');
     for (var i = 0 ; i < theCookies.length; i++) {
-        document.cookie = theCookies[i].split('=')[0] + '=; path=/; expires=Thu, 01 Jan 1970 00:00:01 PST;';
+        document.cookie = theCookies[i].split('=')[0] + "=; path=/; expires=Thu, 01 Jan 1970 00:00:00 PST; secure;";
     }
 }
 
 
+deleteCookies();
+document.cookie = "_ga=; expires= Wed, 19 April 2023 2:59 UTC; path=/; secure";
+document.cookie = "ga; expires= Wed, 19 April 2023 2:59 UTC; path=/; secure";
+document.cookie = "_ga; expires= Wed, 19 April 2023 2:59 UTC; path=/; secure";
+
+document.cookie = "_ga=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+document.cookie = "_gat=; expires=Thu, 01 Jan 2000 00:00::00 UTC; path=/;";
 
 window.addEventListener("load", executeCodes);
+
+
 
 /* 
 In this code, the executeCodes function now checks if the FYI cookie is present. 
